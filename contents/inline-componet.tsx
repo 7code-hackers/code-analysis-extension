4eca3597d2f3adf559ed0be892c2cf0c31b831f1
ygw212 <wangygca@gmail.com>
2023-08-24 22:28:42 -0700

    Update inline-componet.tsx

--- contents/inline-componet.tsx
+++ contents/inline-componet.tsx
@@ -1,7 +1,8 @@
-import type { PlasmoCSConfig, PlasmoGetInlineAnchor  } from "plasmo"
+import cssText from "data-text:~style.css"
+import type { PlasmoCSConfig, PlasmoGetOverlayAnchor } from "plasmo"
+import { useState } from "react"
 
 //tailwind css
-import cssText from "data-text:~style.css"
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
@@ -12,21 +13,35 @@ export const config: PlasmoCSConfig = {
   matches: ["https://github.com/*"]
 }
 
-export const getInlineAnchor: PlasmoGetInlineAnchor  = () =>
-  document.querySelector(`[id="LC10"]`)
+export const getInlineAnchor: PlasmoGetOverlayAnchor = () =>
+  document.querySelector(
+    `#copilot-button-positioner > div.Box-sc-g0xbh4-0.eRkHwF > div > div > div > div.react-line-numbers > div:nth-child(13)`
+  )
 
 // Use this to optimize unmount lookups
 //export const getShadowHostId = () => "plasmo-inline-example-unique-id"
 
 const PlasmoInline = () => {
+  const [showComment, setShowComment] = useState(false)
+  function showCommentHandler() {
+    setShowComment(!showComment)
+  }
   return (
     <div>
       <button
         type="button"
+        onClick={showCommentHandler}
         className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">
-        +
+        comment
       </button>
-
+      {showComment && (
+        <div>
+          <ul role="list" className="divide-y divide-gray-100">
+            <li className="flex justify-between gap-x-6 py-5">c123456</li>
+            <li className="flex justify-between gap-x-6 py-5">c223456789</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
